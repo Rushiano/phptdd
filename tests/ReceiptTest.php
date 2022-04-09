c7abdb39b2f2ebdf34e09eaaf85e8b90701cf7e9
Luciano Lima <eu.lucianolima@gmail.com>
2022-04-09 16:59:34 -0300

    First test with PHPUnit

---
+++ tests/ReceiptTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace TDD\Test;
+
+require dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR."vendor".DIRECTORY_SEPARATOR."autoload.php";
+
+use PHPUnit\Framework\TestCase;
+use TDD\Receipt;
+
+class ReceiptTest extends TestCase
+{
+    public function testTotal()
+    {
+        $receipt = new Receipt();
+        $this->assertEquals(
+            15,
+            $receipt->total([0, 2, 5, 8]),
+            "When summing the total should equel 15"
+        );
+    }
+}

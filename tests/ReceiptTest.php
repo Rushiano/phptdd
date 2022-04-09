748e0502e83e0a49c6e3fc7f31ee0ce3cf9d1845
Luciano Lima <eu.lucianolima@gmail.com>
2022-04-09 17:16:38 -0300

    Making the code on the first test readable

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -8,12 +8,26 @@ use TDD\Receipt;
 
 class ReceiptTest extends TestCase
 {
+    public function setUp()
+    {
+        $this->receipt = new Receipt();
+    }
+
+    //This will unset any instance so PHPUnit doesn't carry anything from one test to the next
+    public function tearDown()
+    {
+        unset($this->Receipt);
+    }
+
     public function testTotal()
     {
-        $receipt = new Receipt();
+        //This is a good practice thing
+        $input = [0, 2, 5, 8];
+        $output = $this->receipt->total($input);
+        
         $this->assertEquals(
             15,
-            $receipt->total([0, 2, 5, 8]),
+            $output,
             "When summing the total should equel 15"
         );
     }

d5c8a5e71de866f84478223ed636ce0b14b0c2e1
Luciano Lima <eu.lucianolima@gmail.com>
2022-04-10 11:28:49 -0300

    Include the phpunit.xml file

--- src/Receipt.php
+++ src/Receipt.php
@@ -2,8 +2,24 @@
 namespace TDD;
 
 class Receipt {
-    public function total(array $items = [])
+    public function total(array $items = [], $coupon)
     {
-        return array_sum($items);
+        $sum = array_sum($items);
+
+        if(!is_null($coupon)){
+            return $sum - ($sum * $coupon);
+        }
+        return $sum;
+    }
+
+    public function tax($amout, $tax)
+    {
+        return ($amout * $tax);
+    }
+
+    public function postTaxTotal($items, $tax, $coupon)
+    {
+        $subtotal = $this->total($items, $coupon);
+        return $subtotal + $this->tax($subtotal, $tax);
     }
 }
--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -16,19 +16,64 @@ class ReceiptTest extends TestCase
     //This will unset any instance so PHPUnit doesn't carry anything from one test to the next
     public function tearDown()
     {
-        unset($this->Receipt);
+        unset($this->receipt);
     }
 
     public function testTotal()
     {
         //This is a good practice thing
         $input = [0, 2, 5, 8];
-        $output = $this->receipt->total($input);
-        
+        $coupon = null;
+        $output = $this->receipt->total($input, $coupon);
+
         $this->assertEquals(
             15,
             $output,
             "When summing the total should equel 15"
         );
+    }
+
+    public function testTotalAndCoupon()
+    {
+        //This is a good practice thing
+        $input = [0, 2, 5, 8];
+        $coupon = 0.20; //This is the DUMMY type of test double
+        $output = $this->receipt->total($input, $coupon);
+
+        $this->assertEquals(
+            12,
+            $output,
+            "When summing the total should equel 12"
+        );
+    }
+
+    //This is the example method of a MOCK type of test double
+    public function testPostTaxTotal()
+    {
+        $receipt = $this->getMockBuilder('TDD\Receipt')
+            ->setMethods(['tax', 'total'])
+            ->getMock();
+        $receipt->method('total')
+            ->will($this->returnValue(10.00));
+        $receipt->method('tax')
+            ->will($this->returnValue(1.00));
+        
+        $result = $receipt->postTaxTotal([1,2,5,8], 0.20, null);
+
+        $this->assertEquals(11.00, $result);
+
+    }
+
+    public function testTax()
+    {
+      $inputAmout = 10.00;
+      $taxInput = 0.10;
+      $output = $this->receipt->tax($inputAmout, $taxInput);
+
+      $this->assertEquals(
+          1.00,
+          $output,
+          "The tax calculation should equal 1.00"
+      );
     }
 }
